03fc635a16dd16e62146088b1150e41ff16f733a
Veeupup <931418134@qq.com>
2020-11-13 12:07:30 +0800

    lab1 sort in armv7 finished

--- lab1/readme.md
+++ lab1/readme.md
@@ -1,17 +1,49 @@
 # sort.s 代码分析
 
+栈中的值
+r7,#4   buffer
+r7      bufferLen
+r7,#20  i
+r7,#16  j
+
 过程参数分析
 
 调用 sort
 ```
 r0 buffer       数组指针
 r1 bufferLen    数组长度
+r7 #sp          栈指针
+buffer r0 保存到 [r7+4]
+bufferLen r1 保存到 [r7]
+r3 i 初始化为 0
+i r3 保存到 [r7+20]
+跳转到 .L2
 ```
 
-进入 sort
-r3 i 初始化为 0
+.L2
+```
+取出 r2 = [r7+20](i)
+取出 r3 = [r7] (bufferLen)
+如果 i < bufferLen，那么跳转到 .L6
+```
 
-跳转到 .L2
+.L6
+```
+保存 r3 到 [r7+16] j
+跳转到 .L3
+```
+
+.L3
+```
+没有 str 没有保存新的值
+r3 = j
+判断 j是否小于 bufferLen - i - 1
+
+```
+
+.L5
+```
+r3 = j
 
 
-
+```
--- lab1/sort.s
+++ lab1/sort.s
@@ -24,49 +24,49 @@ sort:													;/*函数标签*/
 	@ link register save eliminated.
 	push	{r7}
 	sub	sp, sp, #28
-	add	r7, sp, #0
+	add	r7, sp, #0  
 	str	r0, [r7, #4] 				;/*r0为第一个参数buffer，将buffer保存到[r7,#4]*/
 	str	r1, [r7]   						;/*r1为第二个参数bufferlen，保存到[r7]*/
 	movs	r3, #0 									;/* r3为i*/
 	str	r3, [r7, #20] 									;/* 将i保存在[r7,#20]*/
 	b	.L2
 .L6:
-												;/*r3 = 0*/
-	str	r3, [r7, #16] 								;/*[r7+16] = r3*/
+	movs r3, #0											;/*r3 = 0*/
+	str	r3, [r7, #16] 							;/*[r7+16] = r3*/
 	b	.L3
 .L5:   			;/*先判断buffer[j]和buffer[j+1]的大小，然后根据结果来进行是否交换*/
 	ldr	r3, [r7, #16] 					;/*r3 = [r7+16], r3存放着第二层循环，相当于j*/
 	lsls	r3, r3, #2  								;/*r3 = r3*4*/
-				    							;/*r2 = buffer的地址*/
-				      							;/*r3 = r3+r2 ,相当于r3 = buffer[j] */
+	ldr r2, [r7, #4]			    							;/*r2 = buffer的地址*/
+	adds r3, r3, r2			      							;/*r3 = r3+r2 ,相当于r3 = buffer[j] */
 	ldr	r2, [r3]        					;/*r2 = buffer[j] */
-					   					;/*r3 = j*/
-										;/*r3 = r3+1*/
+	ldr r3, [r7, #16]				   	;/*r3 = j*/
+	adds r3, r3, #1									;/*r3 = r3+1*/
 	lsls	r3, r3, #2  						;/*r3 = r3*4*/
-				    					;/*r1 = buffer的地址*/
-										;/*r3 = r3+r1 相当于r3=buffer[j+1]的地址*/
+	ldr r1, [r7, #4]			    					;/*r1 = buffer的地址*/
+	adds r3, r3, r1							;/*r3 = r3+r1 相当于r3=buffer[j+1]的地址*/
 	ldr	r3, [r3]        					;/*r3 = buffer[j+1] */
 	cmp	r2, r3          					;/*buffer[j]和buffer[j+1] */
 	bge	.L4             					;/*当buffer[j]>=buffer[j+1]时，跳到L4，也就是不交换*/
 
 	;/*交换两个数*/
-					   					;/*r3 = j*/
-										;/*r3 = r3+1*/
+	ldr r3, [r7, #16]				   	;/*r3 = j*/
+	adds    r3, r3, #1									;/*r3 = r3+1*/
 	lsls	r3, r3, #2  						;/*r3 = r3*4*/
-				    					;/*r2 = buffer的地址*/
-						      			;/*r3 = r3+r2 相当于r3存放了buffer[j+1]的地址*/
+	ldr r2, [r7, #4]			    	;/*r2 = buffer的地址*/
+	adds r3, r3, r2					      		;/*r3 = r3+r2 相当于r3存放了buffer[j+1]的地址*/
 	ldr	r3, [r3]        					;/*r3 = buffer[j+1] */
-					   				;/* [r7+12]=buffer[j+1]相当于tmp=buffer[j+1] */
-	   									;/*r3 = j*/
-										;/*r3=r3+1,道理同上*/
+	str r3, [r7, #12]				   	;/* [r7+12]=buffer[j+1]相当于tmp=buffer[j+1] */
+	ldr r3, [r7, #16]   				;/*r3 = j*/
+	adds r3, r3, #1									;/*r3=r3+1,道理同上*/
 	lsls	r3, r3, #2  
 	ldr	r2, [r7, #4]    
 	add	r3, r3, r2      					;/*r3存放了buffer[j+1]的地址*/
 	ldr	r2, [r7, #16]   					;/*r2=j*/
 	lsls	r2, r2, #2							
-										;/*r1 = buffer*/    
-										;/*r2 = r2+r1*/
-					 					;/*r2=buffer[j] */
+	ldr r1, [r7, #4]					;/*r1 = buffer*/    
+	adds r2, r2, r1						;/*r2 = r2+r1*/ r2 = j
+	ldr r2, [r1, r2] 				 	;/*r2=buffer[j] */
 	str	r2, [r3]       ;/*因为r3存放了buffer[j+1]的地址，相当于buffer[j+1] = buffer[j] */
 	ldr	r3, [r7, #16]   
 	lsls	r3, r3, #2
@@ -76,9 +76,9 @@ sort:													;/*函数标签*/
 	str	r2, [r3]        					;/*相当于buffer[j] = tmp*/
 
 .L4:										;/*相当于j=j+1*/
-				    					;/*r3=j，将j的值取出给r3*/
-					   					;/*r3=r3+1，j++*/
-					   					;/*将j++写入原来的地址*/
+	ldr r3, [r7, #16]			    					;/*r3=j，将j的值取出给r3*/
+	adds r3, r3, #1				   					;/*r3=r3+1，j++*/
+	str r3, [r7, #16]				   					;/*将j++写入原来的地址*/
 .L3:
 	ldr	r2, [r7]   						;/*r2 = bufferlen*/
 	ldr	r3, [r7, #20] 							;/*r3 = i*/
@@ -95,7 +95,7 @@ sort:													;/*函数标签*/
 .L2:											;/*第一层循环*/
 	ldr	r2, [r7, #20]  							;/* r2=i */
 	ldr	r3, [r7]      							;/*r3=bufferlen*/
-			  								;/*判断第一层循环是否结束*/
+	cmp r2, r3		  							;/*判断第一层循环是否结束 判断 i < bufferLen*/  
 	blt	.L6
 	nop
 	adds	r7, r7, #28  
@@ -106,4 +106,3 @@ sort:													;/*函数标签*/
 	.size	sort, .-sort
 	.ident	"GCC: (Linaro GCC 5.4-2017.05) 5.4.1 20170404"
 	.section	.note.GNU-stack,"",%progbits
-    
